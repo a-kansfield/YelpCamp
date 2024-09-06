1d6cf42ae0b652d800e1da24931e7f234c8bb8cc
Amber Kansfield <amber.kansfield@gmail.com>
2024-09-06 14:14:36 -0500

    Client side error validation with bootstrap and some server side error handling with Express. Installed but have not implemented Joi.

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+
+// Utils
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -26,50 +31,76 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-});
+app.get('/campgrounds', 
+    catchAsync(async (req, res, next) => {
+        const campgrounds = await Campground.find({});
+        res.render('campgrounds/index', { campgrounds });
+    })
+);
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-})
+app.post('/campgrounds', 
+    catchAsync(async (req, res, next) => {
+        if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    })
+);
 
 
 // This must be last in the campgrounds series, because it will treat anything as a potential id and look for it. 
 // In other words, if campgrounds/new were below this, we would never be able to reach it.
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
-});
+app.get('/campgrounds/:id', 
+    catchAsync(async (req, res, next) => {
+        const campground = await Campground.findById(req.params.id);
+        res.render('campgrounds/show', {campground});
+    })
+);
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
+app.get('/campgrounds/:id/edit', 
+    catchAsync(async (req, res, next) => {
+        const campground = await Campground.findById(req.params.id);
+        res.render('campgrounds/edit', {campground});
+    })
+);
+
+app.put('/campgrounds/:id', 
+    catchAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
+        res.redirect(`/campgrounds/${campground._id}`);
+    })
+);
+
+app.delete('/campgrounds/:id', 
+    catchAsync(async (req, res, next) => {
+        const { id } = req.params;
+        await Campground.findByIdAndDelete(id);
+        res.redirect('/campgrounds');
+    })
+);
+app.get('/makecampground', 
+    catchAsync(async (req, res, next) => {
+        const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+        await camp.save();
+        res.send(camp);
+    })
+);
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 })
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.delete('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', {err} );
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp);
-});
+
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
