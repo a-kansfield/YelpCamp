2cb6d4d1d89e80f45bf24c11916061b64454619b
Amber Kansfield <amber.kansfield@gmail.com>
2024-09-05 20:57:21 -0500

    Added in basic bootstrap 5 styles to pages, with random image generation.

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut consectetur quibusdam adipisci vitae sapiente esse quaerat nesciunt, quod porro nobis veniam nisi maiores. Et, illo minima incidunt rem nostrum aliquam!",
+            price
         })
         await camp.save();
     }
